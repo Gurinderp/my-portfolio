8c51536a84118f49bb2d42f2a580ef14250b3931
rednirug <sgurinderpreet@gmail.com>
2020-07-10 23:52:19 -0700

    jumbotron loaded

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./index.css";
+import jumbo from "../../assets/jumbotron-example.png";
 
 function Home() {
 	return (
 		<div>
+			<img
+				src={jumbo}
+				className="img-fluid"
+				onclick=""
+				alt="Responsive image"
+			/>
 			<div>Home</div>
 		</div>
 	);
