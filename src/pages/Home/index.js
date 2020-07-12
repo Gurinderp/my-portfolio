679ec87759059614df1616be6c6914e8df4fb859
rednirug <sgurinderpreet@gmail.com>
2020-07-12 14:22:15 -0700

    Home component index.js restructure

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,14 +5,9 @@ import jumbo from "../../assets/jumbotron-example.png";
 function Home() {
 	return (
 		<div>
-			<img
-				src={jumbo}
-				className="img-fluid"
-				onclick=""
-				alt="Responsive image"
-			></img>
+			<img src={jumbo} className="img-fluid" alt="Responsive image" />
 			<div className="jumbo-overlap">
-				<img className="self-pic" src="#"></img>
+				<img className="self-pic" src="#" />
 				<h3>
 					Sample text to get the ball rolling. Should appear right of
 					the selfie
@@ -20,39 +15,37 @@ function Home() {
 			</div>
 			<div className="media-links">
 				<a href="#">
-					<img src="#">GitHub</img>
+					<img src="#" />
+					<p>GitHub</p>
 				</a>
 				<a href="#">
-					<img src="#">LinkedIn</img>
+					<img src="#" />
+					<p>LinkedIn</p>
 				</a>
 			</div>
 			<div className="skills">
-				<img className="skill-image" src="#"></img>
+				<img className="skill-image" src="#" />
 				<p>Html</p>
-				<img className="skill-image" src="#"></img>
+				<img className="skill-image" src="#" />
 				<p>Html</p>
-				<img className="skill-image" src="#"></img>
+				<img className="skill-image" src="#" />
 				<p>Html</p>
-				<img className="skill-image" src="#"></img>
+				<img className="skill-image" src="#" />
 				<p>Html</p>
 			</div>
 			<div className="project-list">
-				<img className="proj-list" src="#">
-					<a href="#">Deployed Link</a>
-					<a href="#">GitHub Link</a>
-				</img>
-				<img className="proj-list" src="#">
-					<a href="#">Deployed Link</a>
-					<a href="#">GitHub Link</a>
-				</img>
-				<img className="proj-list" src="#">
-					<a href="#">Deployed Link</a>
-					<a href="#">GitHub Link</a>
-				</img>
-				<img className="proj-list" src="#">
-					<a href="#">Deployed Link</a>
-					<a href="#">GitHub Link</a>
-				</img>
+				<img className="proj-list" src="#" />
+				<a href="#">Deployed Link</a>
+				<a href="#">GitHub Link</a>
+				<img className="proj-list" src="#" />
+				<a href="#">Deployed Link</a>
+				<a href="#">GitHub Link</a>
+				<img className="proj-list" src="#" />
+				<a href="#">Deployed Link</a>
+				<a href="#">GitHub Link</a>
+				<img className="proj-list" src="#" />
+				<a href="#">Deployed Link</a>
+				<a href="#">GitHub Link</a>
 			</div>
 			<div className="footer">Footer</div>
 		</div>
