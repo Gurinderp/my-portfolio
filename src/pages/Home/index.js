d0daac763150e497e1b4019762e28ace25a32057
rednirug <sgurinderpreet@gmail.com>
2020-07-12 14:12:10 -0700

    Home component index.js structure

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,8 +10,51 @@ function Home() {
 				className="img-fluid"
 				onclick=""
 				alt="Responsive image"
-			/>
-			<div>Home</div>
+			></img>
+			<div className="jumbo-overlap">
+				<img className="self-pic" src="#"></img>
+				<h3>
+					Sample text to get the ball rolling. Should appear right of
+					the selfie
+				</h3>
+			</div>
+			<div className="media-links">
+				<a href="#">
+					<img src="#">GitHub</img>
+				</a>
+				<a href="#">
+					<img src="#">LinkedIn</img>
+				</a>
+			</div>
+			<div className="skills">
+				<img className="skill-image" src="#"></img>
+				<p>Html</p>
+				<img className="skill-image" src="#"></img>
+				<p>Html</p>
+				<img className="skill-image" src="#"></img>
+				<p>Html</p>
+				<img className="skill-image" src="#"></img>
+				<p>Html</p>
+			</div>
+			<div className="project-list">
+				<img className="proj-list" src="#">
+					<a href="#">Deployed Link</a>
+					<a href="#">GitHub Link</a>
+				</img>
+				<img className="proj-list" src="#">
+					<a href="#">Deployed Link</a>
+					<a href="#">GitHub Link</a>
+				</img>
+				<img className="proj-list" src="#">
+					<a href="#">Deployed Link</a>
+					<a href="#">GitHub Link</a>
+				</img>
+				<img className="proj-list" src="#">
+					<a href="#">Deployed Link</a>
+					<a href="#">GitHub Link</a>
+				</img>
+			</div>
+			<div className="footer">Footer</div>
 		</div>
 	);
 }
