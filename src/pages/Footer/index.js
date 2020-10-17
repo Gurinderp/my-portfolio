ae0cb15d8c1a827bebba3db013bced8ec7e54979
rednirug <sgurinderpreet@gmail.com>
2020-10-16 19:23:18 -0700

    styling change

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -3,10 +3,34 @@ import "./index.css";
 
 function Footer() {
 	return (
-		<div className="footer">
-			<h3>&#169; Gurinderpreet Singh 2020</h3>
+		<div className="footer-container">
+			<div className="about-section bot-sect">
+				<p className="footer-title">About Me</p>
+				<br />
+				<p>
+					I am a Full Stack Developer experimenting with different
+					themes and technologies to improve and increase my skillset.
+				</p>
+				<br />
+				<p>©Gurinderpreet Singh 2020</p>
+			</div>
+			<div className="web-description bot-sect">
+				<p className="footer-title">About This Website</p>
+				<br />
+				<p>
+					This website was made using React. It showcases my skills, abilities, and some projects. Contact is available using the following methods.
+				</p>
+			</div>
+			<div className="contact-container bot-sect">
+				<p className="footer-title">Contact Methods:</p>
+				<br />
+				<ul>
+					<li>Phone: (916) 767-6512</li>
+					<li>Email: sgurinderpreet@gmail.com</li>
+					<li>LinkedIn: www.linkedin.com/in/gurinderpreet-singh</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
-
 export default Footer;
