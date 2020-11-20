f158e39a515dd589cbd1e630d711643e865b56f7
rednirug <sgurinderpreet@gmail.com>
2020-11-19 19:49:01 -0800

    resume changes

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -5,7 +5,22 @@ import pdf from "../../assets/WebDev.pdf";
 function Resume() {
 	return (
 		<div className="container resume-container">
-			<embed src={pdf}></embed>
+			<div className="resume-selection-btns">
+				<button className="resume-skills-btn">Skills</button>
+				<button className="resume-projects-btn">Projects</button>
+				<button className="resume-education-btn">Education</button>
+				<button className="resume-download-btn">Download</button>
+			</div>
+			<div className="resume-skills-section">
+				<p>Skills</p>
+			</div>
+			<div className="hide resume-projects-section">
+				<p>Projects</p>
+			</div>
+			<div className="hide resume-education-section">
+				<p>Education</p>
+			</div>
+			<embed src={pdf} className="hide resume-download-section"></embed>
 		</div>
 	);
 }
